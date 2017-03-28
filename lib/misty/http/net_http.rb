3354925d4ffae36517de3319fdf47372391747ff
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2017-03-28 11:37:17 +0000

    use proxy env if available

--- lib/misty/http/net_http.rb
+++ lib/misty/http/net_http.rb
@@ -3,6 +3,10 @@ module Misty
     module NetHTTP
       def net_http(uri, ssl_verify_mode, log)
         http = Net::HTTP.new(uri.host, uri.port)
+        if ENV['http_proxy']
+          proxy_uri = URI.parse(ENV['http_proxy']) 
+          http = Net::HTTP.new(uri.host, uri.port, proxy_uri.host, proxy_uri.port)
+        end
         http.set_debug_output(log) if log.level == Logger::DEBUG
         if uri.scheme == "https"
           http.use_ssl = true
