f9538a51ea26964a66bb5fd3eb83a9060c7afc49
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2017-04-12 07:20:06 +0000

    moved non proxy http into else block

--- lib/misty/http/net_http.rb
+++ lib/misty/http/net_http.rb
@@ -2,10 +2,11 @@ module Misty
   module HTTP
     module NetHTTP
       def net_http(uri, ssl_verify_mode, log)
-        http = Net::HTTP.new(uri.host, uri.port)
         if ENV['http_proxy']
           proxy_uri = URI.parse(ENV['http_proxy']) 
           http = Net::HTTP.new(uri.host, uri.port, proxy_uri.host, proxy_uri.port)
+        else
+          http = Net::HTTP.new(uri.host, uri.port)
         end
         http.set_debug_output(log) if log.level == Logger::DEBUG
         if uri.scheme == "https"
