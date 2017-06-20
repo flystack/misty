3165b1745425be9fda7e61486cec06ab32ba3fde
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2017-06-20 13:33:04 +0000

    bypass auth by given context

--- lib/misty/auth.rb
+++ lib/misty/auth.rb
@@ -17,8 +17,12 @@ module Misty
     attr_reader :catalog
 
     def self.factory(auth, config)
-      raise URLError, "No URL provided" unless auth[:url] && !auth[:url].empty?
-      http = Misty::HTTP::NetHTTP.net_http(URI.parse(auth[:url]), config.ssl_verify_mode, config.log)
+
+      http = nil
+      if auth[:url]
+        raise URLError, "No URL provided" unless auth[:url] && !auth[:url].empty?
+        http = Misty::HTTP::NetHTTP.net_http(URI.parse(auth[:url]), config.ssl_verify_mode, config.log)
+      end
 
       if auth[:tenant_id] || auth[:tenant]
         return Misty::AuthV2.new(auth, http)
@@ -27,10 +31,18 @@ module Misty
       end
     end
 
-    def initialize(auth, http)
+    def initialize(auth, http = nil)
       @http = http
-      @credentials = set_credentials(auth)
-      @token, @catalog, @expires = set(authenticate)
+      if auth[:context]
+        # bypass the authentication by given token catalog and expire date
+        @token   = auth[:context][:token]
+        @catalog = auth[:context][:catalog]
+        @expires = auth[:context][:expires_at]
+      else
+        # autheticate
+        @credentials = set_credentials(auth)
+        @token, @catalog, @expires = set(authenticate)
+      end
     end
 
     def authenticate
