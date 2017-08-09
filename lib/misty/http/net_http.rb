a3b868de98789e1e1c41ec674597d97fb05ef518
Andreas Pfau <andreas.pfau@sap.com>
2017-08-09 14:16:32 +0200

    allow to provide other connection options to the http client

--- lib/misty/http/net_http.rb
+++ lib/misty/http/net_http.rb
@@ -1,12 +1,15 @@
 module Misty
   module HTTP
     module NetHTTP
-      def self.net_http(endpoint, ssl_verify_mode, log)
+      def self.net_http(endpoint, options, log)
         http = Net::HTTP.new(endpoint.host, endpoint.port)
         http.set_debug_output(log) if log.level == Logger::DEBUG
         if endpoint.scheme == "https"
           http.use_ssl = true
-          http.verify_mode = OpenSSL::SSL::VERIFY_NONE unless ssl_verify_mode
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE unless options[:ssl_verify_mode]
+        end
+        if options[:keep_alive_timeout] && options[:keep_alive_timeout].is_a?(Integer)
+          http.keep_alive_timeout = options[:keep_alive_timeout]
         end
         http
       end
