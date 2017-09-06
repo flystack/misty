flystack/misty

commit 4bff05cde0deee9e53bf94cafd64e0ad7ff7399e

thread safe http requests

lib/misty/http/net_http.rb
@@ -1,20 +1,19 @@
 module Misty
   module HTTP
+    # This class implements the http request routine.
     module NetHTTP
-      @net_http_mutex = Mutex.new
-
-      def self.net_http(endpoint, options, log)
-        @net_http_mutex.synchronize do
-          http = Net::HTTP.new(endpoint.host, endpoint.port)
-          http.set_debug_output(log) if log.level == Logger::DEBUG
-          if endpoint.scheme == "https"
-            http.use_ssl = true
-            http.verify_mode = OpenSSL::SSL::VERIFY_NONE unless options[:ssl_verify_mode]
-          end
-          if options[:keep_alive_timeout] && options[:keep_alive_timeout].is_a?(Integer)
-            http.keep_alive_timeout = options[:keep_alive_timeout]
-          end
-          http
+      def self.http_request(uri, options = {})
+        verify_mode = if options[:ssl_verify_mode]
+                        OpenSSL::SSL::VERIFY_PEER
+                      else
+                        OpenSSL::SSL::VERIFY_NONE
+                      end
+        Net::HTTP.start(
+          uri.host, uri.port,
+          use_ssl: (uri.scheme == 'https'),
+          verify_mode: verify_mode
+        ) do |connection|
+          yield(connection)
         end
       end
     end

lib/misty/auth.rb
@@ -17,54 +17,53 @@ module Misty
     attr_reader :catalog, :token
 
     def self.factory(auth, config)
-      http = nil
-      unless auth[:context]
-        raise URLError, "No URL provided" unless auth[:url] && !auth[:url].empty?
-        connection_options = {
-          ssl_verify_mode: config.ssl_verify_mode,
-          keep_alive_timeout: config.keep_alive_timeout
-        }
-        http = Misty::HTTP::NetHTTP.net_http(URI.parse(auth[:url]), connection_options, config.log)
-      end
-
-      if auth[:tenant_id] || auth[:tenant]
-        return Misty::AuthV2.new(auth, http)
-      else
-        return Misty::AuthV3.new(auth, http)
-      end
+      version = auth[:tenant_id] || auth[:tenant] ? 'V2' : 'V3'
+      klass = Object.const_get("Misty::Auth#{version}")
+      klass.new(auth, config)
     end
 
-    def initialize(auth, http)
+    def initialize(auth, config)
       if auth[:context]
         # bypass the authentication by given token catalog and expire date
         @token   = auth[:context][:token]
         @catalog = auth[:context][:catalog]
         @expires = auth[:context][:expires]
       else
-        @http = http
+        raise URLError, 'No URL provided' if auth[:url].nil? || auth[:url].empty?
+        @uri = URI.parse(auth[:url])
+        @config = config
         # autheticate
         @credentials = set_credentials(auth)
         @token, @catalog, @expires = set(authenticate)
       end
+      #byebug unless config.is_a?(Misty::Cloud::Config)
     end
 
     def authenticate
-      @http.start do |connection|
-        response = connection.post(self.class.path, @credentials.to_json, Misty::HEADER_JSON)
-        raise AuthenticationError, "Response code=#{response.code}, Msg=#{response.msg}" unless response.code =~ /200|201/
+      Misty::HTTP::NetHTTP.http_request(
+        @uri, ssl_verify_mode: @config.ssl_verify_mode, log: @config.log
+      ) do |connection|
+        response = connection.post(self.class.path, @credentials.to_json,
+                                   Misty::HEADER_JSON)
+        unless response.code =~ /200|201/
+          raise AuthenticationError,
+                "Response code=#{response.code}, Msg=#{response.msg}"
+        end
         response
       end
     end
 
     def expired?
-      raise ExpiryError, "Missing token expiration data" if @expires.nil? || @expires.empty?
+      if @expires.nil? || @expires.empty?
+        raise ExpiryError, 'Missing token expiration data'
+      end
       Time.parse(@expires) < Time.now.utc
     end
 
     def get_endpoint(service_names, region, interface)
       @catalog.each do |catalog|
-        if service_names.include? catalog["type"]
-          return catalog_endpoints(catalog["endpoints"], region, interface)
+        if service_names.include? catalog['type']
+          return catalog_endpoints(catalog['endpoints'], region, interface)
         end
       end
       raise CatalogError, "No service found with either #{service_names} name, region #{region}, interface #{interface}"

lib/misty/cloud.rb
@@ -5,8 +5,7 @@ module Misty
   class Cloud
     class Config
       attr_accessor :auth, :content_type, :interface, :log,
-                    :region_id, :ssl_verify_mode, :keep_alive_timeout,
-                    :headers
+                    :region_id, :ssl_verify_mode, :headers
     end
 
     attr_reader :auth
@@ -29,7 +28,6 @@ module Misty
       config.log.level = params[:log_level] ? params[:log_level] : Misty::LOG_LEVEL
       config.region_id = params[:region_id] ? params[:region_id] : Misty::REGION_ID
       config.ssl_verify_mode = params.key?(:ssl_verify_mode) ? params[:ssl_verify_mode] : Misty::SSL_VERIFY_MODE
-      config.keep_alive_timeout = params[:keep_alive_timeout]
       config.headers = params[:headers]
       config
     end

lib/misty/http/client.rb
@@ -8,8 +8,7 @@ module Misty
     class Client
       class Options
         attr_accessor :base_path, :base_url, :interface, :region_id,
-                      :service_names, :ssl_verify_mode, :version,
-                      :keep_alive_timeout, :headers
+                      :service_names, :ssl_verify_mode, :version, :headers
       end
 
       class InvalidDataError < StandardError; end
@@ -55,12 +54,7 @@ module Misty
         @options = setup(options)
         @uri = URI.parse(@auth.get_endpoint(@options.service_names, @options.region_id, @options.interface))
         @base_path = @options.base_path ? @options.base_path : @uri.path
-        @base_path = @base_path.chomp("/")
-        connection_options = {
-          ssl_verify_mode: @options.ssl_verify_mode,
-          keep_alive_timeout: @options.keep_alive_timeout
-        }
-        @http = Misty::HTTP::NetHTTP.net_http(@uri, connection_options, @config.log)
+        @base_path = @base_path.chomp('/')
         @version = nil
         @microversion = false
       end
@@ -73,7 +67,7 @@ module Misty
       # and Service API has "/v1/{tenant_id}/stacks"
       # then the path prefix is ignored and path is only "/stacks"
       def self.prefix_path_to_ignore
-        ""
+        ''
       end
 
       def headers_default
@@ -81,7 +75,7 @@ module Misty
       end
 
       def headers
-        h = headers_default.merge("X-Auth-Token" => "#{@auth.get_token}")
+        h = headers_default.merge('X-Auth-Token' => @auth.get_token.to_s)
         h.merge!(microversion_header) if microversion
         h.merge!(@options.headers) if @options.headers
         h
@@ -101,7 +95,6 @@ module Misty
         options.region_id           = params[:region_id]       ? params[:region_id] : @config.region_id
         options.service_names       = params[:service_name]    ? self.class.service_names << params[:service_name] : self.class.service_names
         options.ssl_verify_mode     = params[:ssl_verify_mode] ? params[:ssl_verify_mode] : @config.ssl_verify_mode
-        options.keep_alive_timeout  = params[:keep_alive_timeout] ? params[:keep_alive_timeout] : @config.keep_alive_timeout
         options.headers             = params[:headers] ? params[:headers] : @config.headers
         options.version             = params[:version]         ? params[:version] : "CURRENT"
 

lib/misty/http/request.rb
@@ -2,17 +2,19 @@ module Misty
   module HTTP
     module Request
       def decode?(response)
-        if @config.content_type != :json && response.code =~ /2??/  && !response.is_a?(Net::HTTPNoContent) \
-          && !response.is_a?(Net::HTTPResetContent) && response.header["content-type"] \
-          && response.header["content-type"].include?("application/json")
+        if @config.content_type != :json && response.code =~ /2??/ && !response.is_a?(Net::HTTPNoContent) \
+          && !response.is_a?(Net::HTTPResetContent) && response.header['content-type'] \
+          && response.header['content-type'].include?('application/json')
           true
         end
       end
 
       def http(request)
-        @http.start do |connection|
+        Misty::HTTP::NetHTTP.http_request(
+          @uri, ssl_verify_mode: @options.ssl_verify_mode, log: @config.log
+        ) do |connection|
           response = connection.request(request)
-          response.body = JSON.load(response.body) if decode?(response)
+          response.body = JSON.parse(response.body) if decode?(response)
           response
         end
       end

lib/misty/openstack/microversion.rb
@@ -50,10 +50,10 @@ module Misty
       end
 
       def versions_fetch
-        request = Net::HTTP::Get.new("/", headers_default)
-        response = @http.request request
+        response = http_get('/', headers_default)
         raise VersionError, "Code: #{response.code}, Message: #{response.msg}" unless response.code =~ /2??/
-        list = JSON.load(response.body)["versions"]
+        data = response.body.is_a?(Hash) ? response.body : JSON.parse(response.body)
+        list = data['versions']
         raise VersionError, "Missing version data" unless list
         list
       end

test/unit/auth_test.rb
@@ -2,9 +2,8 @@ require 'test_helper'
 require 'auth_helper'
 
 describe Misty::Auth do
-  let(:http) do
-    endpoint = URI.parse("http://localhost:5000")
-    Net::HTTP.new(endpoint.host, endpoint.port)
+  let(:config) do
+    Misty::Cloud::Config.new
   end
 
   describe Misty::AuthV3 do
@@ -14,8 +13,8 @@ describe Misty::Auth do
         to_return(:status => 200, :body => "{\"token\":{\"catalog\":[]}}", :headers => {"x-subject-token"=>"token_data"})
 
         proc do
-          Misty::AuthV3.new({}, http)
-        end.must_raise Misty::Auth::CredentialsError
+          Misty::AuthV3.new({}, config)
+        end.must_raise Misty::Auth::URLError
       end
 
       describe "using the password method" do
@@ -32,7 +31,7 @@ describe Misty::Auth do
               with(:body => "{\"auth\":{\"identity\":{\"methods\":[\"password\"],\"password\":{\"user\":{\"id\":\"user_id\",\"password\":\"secret\"}}},\"scope\":{\"project\":{\"id\":\"project_id\"}}}}").
               to_return(:status => 200, :body => JSON.dump(auth_response_v3("identity", "keystone")), :headers => {"x-subject-token"=>"token_data"})
 
-            Misty::AuthV3.new(auth, http)
+            Misty::AuthV3.new(auth, config)
           end
 
           it "authenticates using a project name and a project domain id" do
@@ -48,7 +47,7 @@ describe Misty::Auth do
               with(:body => "{\"auth\":{\"identity\":{\"methods\":[\"password\"],\"password\":{\"user\":{\"id\":\"user_id\",\"password\":\"secret\"}}},\"scope\":{\"project\":{\"name\":\"project\",\"domain\":{\"id\":\"domain_id\"}}}}}").
               to_return(:status => 200, :body => JSON.dump(auth_response_v3("identity", "keystone")), :headers => {"x-subject-token"=>"token_data"})
 
-            Misty::AuthV3.new(auth, http)
+            Misty::AuthV3.new(auth, config)
           end
         end
 
@@ -65,7 +64,7 @@ describe Misty::Auth do
               with(:body => "{\"auth\":{\"identity\":{\"methods\":[\"password\"],\"password\":{\"user\":{\"id\":\"user_id\",\"password\":\"secret\"}}},\"scope\":{\"domain\":{\"id\":\"domain_id\"}}}}").
               to_return(:status => 200, :body => JSON.dump(auth_response_v3("identity", "keystone")), :headers => {"x-subject-token"=>"token_data"})
 
-            Misty::AuthV3.new(auth, http)
+            Misty::AuthV3.new(auth, config)
           end
 
           it "authenticates using a domain name" do
@@ -80,7 +79,7 @@ describe Misty::Auth do
               with(:body => "{\"auth\":{\"identity\":{\"methods\":[\"password\"],\"password\":{\"user\":{\"id\":\"user_id\",\"password\":\"secret\"}}},\"scope\":{\"domain\":{\"name\":\"domain\"}}}}").
               to_return(:status => 200, :body => JSON.dump(auth_response_v3("identity", "keystone")), :headers => {"x-subject-token"=>"token_data"})
 
-            Misty::AuthV3.new(auth, http)
+            Misty::AuthV3.new(auth, config)
           end
         end
       end
@@ -98,7 +97,7 @@ describe Misty::Auth do
               with(:body => "{\"auth\":{\"identity\":{\"methods\":[\"token\"],\"token\":{\"id\":\"token\"}},\"scope\":{\"project\":{\"id\":\"project_id\"}}}}").
               to_return(:status => 200, :body => JSON.dump(auth_response_v3("identity", "keystone")), :headers => {"x-subject-token"=>"token_data"})
 
-            Misty::AuthV3.new(auth, http)
+            Misty::AuthV3.new(auth, config)
           end
 
           it "authenticates using a project name and a project domain id" do
@@ -113,7 +112,7 @@ describe Misty::Auth do
               with(:body => "{\"auth\":{\"identity\":{\"methods\":[\"token\"],\"token\":{\"id\":\"token\"}},\"scope\":{\"project\":{\"name\":\"project\",\"domain\":{\"id\":\"domain_id\"}}}}}").
               to_return(:status => 200, :body => JSON.dump(auth_response_v3("identity", "keystone")), :headers => {"x-subject-token"=>"token_data"})
 
-            Misty::AuthV3.new(auth, http)
+            Misty::AuthV3.new(auth, config)
           end
         end
 
@@ -129,7 +128,7 @@ describe Misty::Auth do
               with(:body => "{\"auth\":{\"identity\":{\"methods\":[\"token\"],\"token\":{\"id\":\"token\"}},\"scope\":{\"domain\":{\"id\":\"domain_id\"}}}}").
               to_return(:status => 200, :body => JSON.dump(auth_response_v3("identity", "keystone")), :headers => {"x-subject-token"=>"token_data"})
 
-            Misty::AuthV3.new(auth, http)
+            Misty::AuthV3.new(auth, config)
           end
 
           it "authenticates using a domain name" do
@@ -143,7 +142,7 @@ describe Misty::Auth do
               with(:body => "{\"auth\":{\"identity\":{\"methods\":[\"token\"],\"token\":{\"id\":\"token\"}},\"scope\":{\"domain\":{\"name\":\"domain\"}}}}").
               to_return(:status => 200, :body => JSON.dump(auth_response_v3("identity", "keystone")), :headers => {"x-subject-token"=>"token_data"})
 
-            Misty::AuthV3.new(auth, http)
+            Misty::AuthV3.new(auth, config)
           end
         end
       end
@@ -164,7 +163,7 @@ describe Misty::Auth do
         stub_request(:post, "http://localhost:5000/v3/auth/tokens").
         to_return(:status => 200, :body => "{\"token\":{\"catalog\":[\"catalog_data\"]}}", :headers => {"x-subject-token"=>"token_data"})
 
-        auth = Misty::AuthV3.new(authv3_creds, http)
+        auth = Misty::AuthV3.new(authv3_creds, config)
         auth.stub :expired?, false do
           auth.get_token.must_equal "token_data"
         end
@@ -174,7 +173,7 @@ describe Misty::Auth do
         stub_request(:post, "http://localhost:5000/v3/auth/tokens").
         to_return(:status => 200, :body => "{\"token\":{\"catalog\":[\"catalog_data\"]}}", :headers => {"x-subject-token"=>"token_data"})
 
-        auth = Misty::AuthV3.new(authv3_creds, http)
+        auth = Misty::AuthV3.new(authv3_creds, config)
         auth.catalog.must_equal ["catalog_data"]
       end
 
@@ -182,7 +181,7 @@ describe Misty::Auth do
         stub_request(:post, "http://localhost:5000/v3/auth/tokens").
         to_return(:status => 200, :body => JSON.dump(auth_response_v3("identity", "keystone")), :headers => {"x-subject-token"=>"token_data"})
 
-        auth = Misty::AuthV3.new(authv3_creds, http)
+        auth = Misty::AuthV3.new(authv3_creds, config)
         auth.get_endpoint(%w{identity}, "regionOne", "public").must_equal "http://localhost"
       end
     end
@@ -195,8 +194,8 @@ describe Misty::Auth do
           to_return(:status => 200, :body => "{\"access\":{\"token\":{\"id\":\"token_data\"}}}", :headers => {})
 
         proc do
-          Misty::AuthV2.new({}, http)
-        end.must_raise Misty::Auth::CredentialsError
+          Misty::AuthV2.new({}, config)
+        end.must_raise Misty::Auth::URLError
       end
 
       describe "using the password method" do
@@ -212,7 +211,7 @@ describe Misty::Auth do
             with(:body => "{\"auth\":{\"passwordCredentials\":{\"username\":\"user\",\"password\":\"secret\"},\"tenantName\":\"tenant\"}}").
             to_return(:status => 200, :body => JSON.dump(auth_response_v2("identity", "keystone")), :headers => {})
 
-          Misty::AuthV2.new(auth, http)
+          Misty::AuthV2.new(auth, config)
         end
 
         it "authenticates using the tenant id" do
@@ -227,7 +226,7 @@ describe Misty::Auth do
             with(:body => "{\"auth\":{\"passwordCredentials\":{\"username\":\"user\",\"password\":\"secret\"},\"tenantId\":\"tenant_id\"}}").
             to_return(:status => 200, :body => JSON.dump(auth_response_v2("identity", "keystone")), :headers => {})
 
-          Misty::AuthV2.new(auth, http)
+          Misty::AuthV2.new(auth, config)
         end
       end
 
@@ -243,7 +242,7 @@ describe Misty::Auth do
             with(:body => "{\"auth\":{\"token\":{\"id\":\"token_id\"},\"tenantName\":\"tenant\"}}").
             to_return(:status => 200, :body => JSON.dump(auth_response_v2("identity", "keystone")), :headers => {})
 
-          Misty::AuthV2.new(auth, http)
+          Misty::AuthV2.new(auth, config)
         end
 
         it "authenticates using the tenant id" do
@@ -257,7 +256,7 @@ describe Misty::Auth do
             with(:body => "{\"auth\":{\"token\":{\"id\":\"token_id\"},\"tenantId\":\"tenant_id\"}}").
             to_return(:status => 200, :body => JSON.dump(auth_response_v2("identity", "keystone")), :headers => {})
 
-          Misty::AuthV2.new(auth, http)
+          Misty::AuthV2.new(auth, config)
         end
       end
     end
@@ -276,7 +275,7 @@ describe Misty::Auth do
         stub_request(:post, "http://localhost:5000/v2.0/tokens").
           to_return(:status => 200, :body => "{\"access\":{\"token\":{\"id\":\"token_data\"},\"serviceCatalog\":[\"catalog_data\"]}}", :headers => {})
 
-        auth = Misty::AuthV2.new(authv2_creds, http)
+        auth = Misty::AuthV2.new(authv2_creds, config)
         auth.stub :expired?, false do
           auth.get_token.must_equal "token_data"
         end
@@ -286,7 +285,7 @@ describe Misty::Auth do
         stub_request(:post, "http://localhost:5000/v2.0/tokens").
           to_return(:status => 200, :body => "{\"access\":{\"token\":{\"id\":\"token_data\"},\"serviceCatalog\":[\"catalog_data\"]}}", :headers => {})
 
-        auth = Misty::AuthV2.new(authv2_creds, http)
+        auth = Misty::AuthV2.new(authv2_creds, config)
         auth.catalog.must_equal ["catalog_data"]
       end
 
@@ -294,7 +293,7 @@ describe Misty::Auth do
         stub_request(:post, "http://localhost:5000/v2.0/tokens").
           to_return(:status => 200, :body => JSON.dump(auth_response_v2("identity", "keystone")), :headers => {"x-subject-token"=>"token_data"})
 
-        auth = Misty::AuthV2.new(authv2_creds, http)
+        auth = Misty::AuthV2.new(authv2_creds, config)
         auth.get_endpoint(%w{identity}, "regionOne", "public").must_equal "http://localhost"
       end
     end

test/unit/http/client_test.rb
@@ -39,13 +39,6 @@ describe Misty::HTTP::Client do
     end
   end
 
-  describe "#net_http" do
-    it "returns a Net/http instance" do
-      endpoint = URI.parse("http://localhost")
-      Misty::HTTP::NetHTTP.net_http(endpoint, {ssl_verify_mode: false}, Logger.new("/dev/null")).must_be_instance_of Net::HTTP
-    end
-  end
-
   describe "#setup" do
     it "sets default options" do
       options = service.send(:setup, {})

commit 8e4ea81862e7ca1991af3cc54f4fd8b516b82e63

fix usage of proxy environment variables
This has been broken in 4bff05cde0deee9e53bf94cafd64e0ad7ff7399e.

lib/misty/http/net_http.rb
@@ -11,7 +11,7 @@ module Misty
           end
         end
 
-        Net::HTTP.start(uri.host, uri.port, http_options) do |connection|
+        Net::HTTP.start(uri.host, uri.port, :ENV, http_options) do |connection|
           yield(connection)
         end
       end