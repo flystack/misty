0be13d781d4493ada51f133a2e4a620eb0097a63
Gilles Dubreuil <gilles@redhat.com>
2018-03-07 16:25:41 +1100

    Authentication: Fix credentials for user_domain_name

--- lib/misty/auth/auth_v3.rb
+++ lib/misty/auth/auth_v3.rb
@@ -48,10 +48,11 @@ module Misty
 
     def set_credentials(auth)
       if auth[:project_id] || auth[:project]
-        # scope: project
-        project_domain_id = auth[:project_domain_id]
-        if project_domain_id.nil? && auth[:project_domain].nil?
+        if auth[:project_domain_id].nil? && auth[:project_domain].nil?
           project_domain_id = Misty::DOMAIN_ID
+        else
+          project_domain_id = auth[:project_domain_id] if auth[:project_domain_id]
+          project_domain = auth[:project_domain] if auth[:project_domain]
         end
 
         @project = Misty::Auth::ProjectScope.new(auth[:project_id], auth[:project])
@@ -66,13 +67,21 @@ module Misty
         end
       end
 
+      # TODO: Restore :token option
       if auth[:token]
         @token = auth[:token]
       else
-        user_domain_id = auth[:user_domain_id] ? auth[:user_domain_id] : Misty::DOMAIN_ID
         @user = Misty::Auth::User.new(auth[:user_id], auth[:user])
+
+        if auth[:user_domain_id].nil? && auth[:user_domain].nil?
+          user_domain_id = Misty::DOMAIN_ID
+        else
+          user_domain_id = auth[:user_domain_id] if auth[:user_domain_id]
+          user_domain = auth[:user_domain] if auth[:user_domain]
+        end
+
+        @user.domain = Misty::Auth::Name.new(user_domain_id, user_domain)
         @user.password = auth[:password]
-        @user.domain = Misty::Auth::Name.new(user_domain_id, auth[:user_domain])
       end
 
       credentials
